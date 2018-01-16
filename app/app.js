cb88d0a23c19b80c3fd771590bbf94f0493014c8
Patrick Biffle <pbiffle@gmail.com>
2018-01-16 13:27:25 -0700

    added my array of objects

--- app/app.js
+++ app/app.js
@@ -1,7 +1,21 @@
-let arrayOfObj = [{
-    name: "Dan",
-    age: "21"
-},{
-    name: "Ralph",
-    age: "50"
-}];
+let arrayOfObj = [
+	{
+		name: 'Dan',
+		age: '21'
+	},
+	{
+		name: 'Ralph',
+		age: '50'
+	}
+]
+
+const $arrayOfObj = [
+	{
+		name: 'Poodle',
+		age: 2
+	},
+	{
+		name: 'Labradoodle',
+		age: 27
+	}
+]
